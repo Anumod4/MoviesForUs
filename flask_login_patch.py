975f5cc1027e92e75079923967e9a3e9b6896353
Anumod4 <anumod@example.com>
2025-01-03 23:42:39 +0530

    Update flask_login_patch with error handling

--- flask_login_patch.py
+++ flask_login_patch.py
@@ -1,4 +1,5 @@
 # Patch for Flask-Login to use urllib.parse instead of Werkzeug URL decoding
+import logging
 from urllib.parse import parse_qs
 
 def url_decode(query_string, *args, **kwargs):
@@ -14,5 +15,10 @@ def url_decode(query_string, *args, **kwargs):
     return parse_qs(query_string, *args, **kwargs)
 
 # Monkey patch flask_login.utils to use our custom url_decode
-import flask_login.utils
-flask_login.utils.url_decode = url_decode
+try:
+    import flask_login.utils
+    flask_login.utils.url_decode = url_decode
+    logging.info("Successfully patched flask_login.utils.url_decode")
+except ImportError as e:
+    logging.error(f"Could not import flask_login.utils: {e}")
+    raise
